238f5c92b8ed35796478cdfda944e38050fb8edb
chibiharu <chibiharujijimasa@gmail.com>
2022-01-19 13:43:39 +0000

    fix:NWを中心とした大幅な仕様変更

--- script/create_stack.sh
+++ script/create_stack.sh
@@ -2,9 +2,10 @@
 
 
 # スタック作成
-#aws cloudformation create-stack --stack-name cfn-dev-network-stack --template-body file://./network.yml
+#aws cloudformation create-stack --stack-name cfn-dev-network-stack --template-body file://../network.yml
 
-#aws cloudformation create-stack --stack-name cfn-dev-security-stack --template-body file://./security.yml
+#aws cloudformation create-stack --stack-name cfn-dev-security-stack --template-body file://../security.yml
 
-aws cloudformation create-stack --stack-name cfn-dev-server-stac --template-body file://./server.yml \
---parameters ParameterKey=KeyName,ParameterValue=chibiharu.systems.dev.key
+#aws cloudformation create-stack --stack-name cfn-dev-server-stac --template-body file://../server.yml --parameters ParameterKey=KeyName,ParameterValue=chibiharu.systems.dev.key
+
+aws cloudformation create-stack --stack-name cfn-dev-database-stack --template-body file://../database.yml
--- script/validate.sh
+++ script/validate.sh
@@ -2,5 +2,10 @@
 #https://none06.hatenadiary.org/entry/2021/07/10/041711
 #aws cloudformation validate-template --template-body file://./server.yml
 
-aws cloudformation create-stack --stack-name cfn-dev-server-stac --template-body file://./server.yml \
---parameters ParameterKey=KeyName,ParameterValue=chibiharu.systems.dev.key
+#aws cloudformation create-stack --stack-name cfn-dev-server-stac --template-body file://./server.yml \
+#--parameters ParameterKey=KeyName,ParameterValue=chibiharu.systems.dev.key
+
+
+aws cloudformation validate-template --template-body file://../network.yml
+
+
