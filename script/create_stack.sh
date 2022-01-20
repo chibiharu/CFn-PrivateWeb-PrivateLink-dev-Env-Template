20c2aa9a669776adc857fdf4df754521d61b2bc2
chibiharu <chibiharujijimasa@gmail.com>
2022-01-20 15:40:40 +0000

    fix:関数挿入

--- script/create_stack.sh
+++ script/create_stack.sh
@@ -2,10 +2,12 @@
 
 
 # スタック作成
-#aws cloudformation create-stack --stack-name cfn-dev-network-stack --template-body file://../network.yml
+#aws cloudformation create-stack --stack-name cfn-dev-network-stack --template-body file://../network.yml \
+#--parameters ParameterKey=PJPrefix,ParameterValue=QiitaWebEnv
 
-#aws cloudformation create-stack --stack-name cfn-dev-security-stack --template-body file://../security.yml
+aws cloudformation create-stack --stack-name cfn-dev-security-stack --template-body file://../security.yml \
+--parameters ParameterKey=PJPrefix,ParameterValue=QiitaWebEnv
 
 #aws cloudformation create-stack --stack-name cfn-dev-server-stac --template-body file://../server.yml --parameters ParameterKey=KeyName,ParameterValue=chibiharu.systems.dev.key
 
-aws cloudformation create-stack --stack-name cfn-dev-database-stack --template-body file://../database.yml
+#aws cloudformation create-stack --stack-name cfn-dev-database-stack --template-body file://../database.yml
