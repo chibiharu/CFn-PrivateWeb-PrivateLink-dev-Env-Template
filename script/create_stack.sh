d3088abbea51df98afe78eb9b0dd143a1778e015
chibiharu <chibiharujijimasa@gmail.com>
2022-01-23 11:03:57 +0000

    add:database.ymlの追加[C

--- script/create_stack.sh
+++ script/create_stack.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 
-# スタック作成
-#aws cloudformation create-stack --stack-name cfn-dev-network-stack --template-body file://../network.yml \
-#--parameters ParameterKey=PJPrefix,ParameterValue=QiitaWebEnv
+### スタック作成 ###
+aws cloudformation create-stack \
+--stack-name cfn-dev-server-stac \
+--template-body file://../server.yml \
+--parameters ParameterKey=KeyName,ParameterValue=<キー名> \
+ParameterKey=PJPrefix,ParameterValue=QiitaWebEnv --capabilities CAPABILITY_NAMED_IAM
 
-aws cloudformation create-stack --stack-name cfn-dev-security-stack --template-body file://../security.yml \
---parameters ParameterKey=PJPrefix,ParameterValue=QiitaWebEnv
-
-#aws cloudformation create-stack --stack-name cfn-dev-server-stac --template-body file://../server.yml --parameters ParameterKey=KeyName,ParameterValue=chibiharu.systems.dev.key
-
-#aws cloudformation create-stack --stack-name cfn-dev-database-stack --template-body file://../database.yml
--- script/validate.sh
+++ script/validate.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-#https://none06.hatenadiary.org/entry/2021/07/10/041711
-#aws cloudformation validate-template --template-body file://./server.yml
-
-#aws cloudformation create-stack --stack-name cfn-dev-server-stac --template-body file://./server.yml \
-#--parameters ParameterKey=KeyName,ParameterValue=chibiharu.systems.dev.key
 
 
-aws cloudformation validate-template --template-body file://../network.yml
+### テンプレートの構文確認 ###
+aws cloudformation validate-template --template-body file://../xxxxx.yml
 
 
